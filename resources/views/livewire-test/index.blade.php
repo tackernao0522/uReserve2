0aab7c5f35c2a9b1e70c5a9e860a16fa98a2d9a3
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-07 15:50:53 +0900

    サンプル Counter

--- resources/views/livewire-test/index.blade.php
+++ resources/views/livewire-test/index.blade.php
@@ -1,1 +1,16 @@
-livewireテスト
+<html>
+
+<head>
+    @livewireStyles
+</head>
+
+<body>
+    <div style="text-align: center">livewireテスト</div>
+    <br>
+    {{-- <livewire:counter /> --}}
+    @livewire('counter')
+
+    @livewireScripts
+</body>
+
+</html>
