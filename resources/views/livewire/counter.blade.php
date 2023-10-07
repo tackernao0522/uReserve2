cbc63b7a0202543097530a7891d857759372a40e
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-07 16:41:29 +0900

    wire:modelのオプション

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -2,7 +2,10 @@
     <button wire:click="increment">+</button>
     <h1>{{ $count }}</h1>
     <div class="mb-8"></div>
-    <input type="text" wire:model="name">
+    {{-- <input type="text" wire:model="name"> --}}
+    {{-- <input type="text" wire:model.debounce.2000ms="name"> --}}
+    <input type="text" wire:model.lazy="name">
+    {{-- <input type="text" wire:model.defer="name"> --}}
     <br>
     こんにちは、{{ $name }} さん
 </div>
