6bfe6ec50d629a76db393382ae685f5ea88bcf31
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-07 17:33:10 +0900

    ライフサイクルフック (mount, updated)

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -2,9 +2,9 @@
     <button wire:click="increment">+</button>
     <h1>{{ $count }}</h1>
     <div class="mb-8"></div>
-    {{-- <input type="text" wire:model="name"> --}}
+    <input type="text" wire:model="name">
     {{-- <input type="text" wire:model.debounce.2000ms="name"> --}}
-    <input type="text" wire:model.lazy="name">
+    {{-- <input type="text" wire:model.lazy="name"> --}}
     {{-- <input type="text" wire:model.defer="name"> --}}
     <br>
     こんにちは、{{ $name }} さん
