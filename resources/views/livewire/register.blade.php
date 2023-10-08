4165b4440af46c1aed650f2058ded50bac9c7505
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-08 12:44:00 +0900

    とwire:submit.prevent

--- resources/views/livewire/register.blade.php
+++ resources/views/livewire/register.blade.php
@@ -1,5 +1,17 @@
 <div style="text-align: center">
-    <form wire:submit="register">
+    <form wire:submit.prevent="register">
+        <label for="name">名前</label>
+        <input type="text" id="name" wire:model="name">
+        <br>
+        <br>
+        <label for="email">メールアドレス</label>
+        <input type="email" id="email" wire:model="email">
+        <br>
+        <br>
+        <label for="password">パスワード</label>
+        <input type="password" id="password" wire:model="password">
+        <br>
+        <br>
         <button>登録する</button>
     </form>
 </div>
