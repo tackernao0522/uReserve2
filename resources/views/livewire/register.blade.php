4e7cc4645149a4efaf126e9161aeef5cbc234808
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-08 14:29:26 +0900

    バリデーションメッセージ

--- resources/views/livewire/register.blade.php
+++ resources/views/livewire/register.blade.php
@@ -3,14 +3,23 @@
         <label for="name">名前</label>
         <input type="text" id="name" wire:model="name">
         <br>
+        @error('name')
+            <div style="color: red">{{ $message }}</div>
+        @enderror
         <br>
         <label for="email">メールアドレス</label>
         <input type="email" id="email" wire:model="email">
         <br>
+        @error('email')
+            <div style="color: red;">{{ $message }}</div>
+        @enderror
         <br>
         <label for="password">パスワード</label>
         <input type="password" id="password" wire:model="password">
         <br>
+        @error('password')
+            <div style="color: red;">{{ $message }}</div>
+        @enderror
         <br>
         <button>登録する</button>
     </form>
