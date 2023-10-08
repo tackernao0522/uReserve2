afdcef008a5fc57ccd0ac00e0a78ccde470176b6
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-08 14:52:51 +0900

    Tailwindcssの読み込み

--- resources/views/livewire/register.blade.php
+++ resources/views/livewire/register.blade.php
@@ -1,26 +1,26 @@
-<div style="text-align: center">
+<div class="text-center">
     <form wire:submit.prevent="register">
         <label for="name">名前</label>
         <input type="text" id="name" wire:model="name">
         <br>
         @error('name')
-            <div style="color: red">{{ $message }}</div>
+            <div class="text-red-400">{{ $message }}</div>
         @enderror
         <br>
         <label for="email">メールアドレス</label>
         <input type="email" id="email" wire:model="email">
         <br>
         @error('email')
-            <div style="color: red;">{{ $message }}</div>
+            <div class="text-red-400">{{ $message }}</div>
         @enderror
         <br>
         <label for="password">パスワード</label>
         <input type="password" id="password" wire:model="password">
         <br>
         @error('password')
-            <div style="color: red;">{{ $message }}</div>
+            <div class="text-red-400">{{ $message }}</div>
         @enderror
         <br>
-        <button>登録する</button>
+        <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded">登録する</button>
     </form>
 </div>
