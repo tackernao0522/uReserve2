a0a49b2903d592d4cac2f9dcf65a260fbf7f5e0d
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-08 11:59:06 +0900

    アクション wire:click, wire:mouseoverなど

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -2,10 +2,13 @@
     <button wire:click="increment">+</button>
     <h1>{{ $count }}</h1>
     <div class="mb-8"></div>
+    こんにちは、{{ $name }} さん
+    <br>
     <input type="text" wire:model="name">
     {{-- <input type="text" wire:model.debounce.2000ms="name"> --}}
     {{-- <input type="text" wire:model.lazy="name"> --}}
     {{-- <input type="text" wire:model.defer="name"> --}}
     <br>
-    こんにちは、{{ $name }} さん
+    <br>
+    <button wire:mouseover="mouseOver">マウスを合わせてね</button>
 </div>
