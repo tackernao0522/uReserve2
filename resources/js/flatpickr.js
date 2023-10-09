cba55d099890fce68f44aca875c9e3817a8df5d3
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-09 15:41:42 +0900

    flatpickr 日本語化・時間表示

--- resources/js/flatpickr.js
+++ resources/js/flatpickr.js
@@ -1,3 +1,24 @@
-import flatpickr from 'flatpickr'
+import flatpickr from "flatpickr";
+import { Japanese } from "flatpickr/dist/l10n/ja.js";
 
-flatpickr('#event_date', {})
+// 日本語設定、今日以降選択、30日間
+flatpickr("#event_date", {
+  locale: Japanese,
+  minDate: "today",
+  maxDate: new Date().fp_incr(30)
+});
+
+// 時間表示、絡んだ-補表示、24時間表記
+const setting = {
+  locale: Japanese,
+  enableTime: true,
+  noCalendar: true,
+  dateFormat: "H:i",
+  time_24hr: true,
+  minTime: "10:00",
+  maxTime: "20:00"
+};
+
+flatpickr("#start_time", setting);
+
+flatpickr("#end_time", setting);
