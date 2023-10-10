8f9627513a421315cfd949fd430946a02933c517
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-10 18:40:21 +0900

    イベント保存時の重複チェッく サービスへの切り離し

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -3,10 +3,20 @@
 namespace App\Services;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class EventService
 {
-    public static function parseDateTime($eventDate, $startTime, $endTime)
+    public static function checkEventDuplication($eventDate, $startTime, $endTime)
+    {
+        return DB::table('events')
+            ->whereDate('start_date', $eventDate)
+            ->whereTime('end_date', '>', $startTime)
+            ->whereTime('start_date', '<', $endTime)
+            ->exists();
+    }
+
+    public static function joinDateAndTime($eventDate, $startTime, $endTime)
     {
         $start = $eventDate . ' ' . $startTime;
         $startDate = Carbon::createFromFormat('Y-m-d H:i', $start);
