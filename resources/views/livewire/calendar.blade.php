06c1e02b33fc951511905ff3b03c8ed62c459ba2
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-18 19:07:45 +0900

    87 Blade component(daym calendar-time)

--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -1,7 +1,20 @@
 <div>
-    カレンダー
-    <input type="text" class="block mt-1 w-full" id="calendar" name="calendar" value="{{ $currentDate }}"
+    <div class="text-center text-sm">
+        日付を選択してください。本日から最大30日先まで選択可能です。
+    </div>
+    <input type="text" id="calendar" class="block mt-1 mb-2 mx-auto" name="calendar" value="{{ $currentDate }}"
         wire:change="getDate($event.target.value)">
+
+    <div class="flex border border-green-400 mx-auto">
+        <x-calendar-time />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+    </div>
     <div class="flex">
         @for ($day = 0; $day < 7; $day++)
             {{ $currentWeek[$day] }}
